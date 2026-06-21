3fd4926bfdf75f4b638491ce114faf729a65fcb1
jazeggelaar <jazeggelaar@gmail.com>
2026-06-21 07:40:08 +0000

    added if

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import streamlit as st #jesse-calculator.streamlit.app
+rules_visibility = True
 st.title("calculator!")
 math = st.text_input("type here your math question")
 if st.button("run math"):
     answer = eval(math)
     st.write("calculated:", answer)
-st.write("rules:")
-st.write("| unsupported:")
-st.write("| | letters")
-st.write("| | symbols")
-st.write("| supported:")
-st.write("| | /")
-st.write("| | *")
-st.write("| | -")
-st.write("| | +")
-st.write("| | nummbers")
+    rules_visibility = False
+if rules_visibility:
+    st.write("rules:")
+    st.write("| unsupported:")
+    st.write("| | letters")
+    st.write("| | symbols")
+    st.write("| supported:")
+    st.write("| | /")
+    st.write("| | *")
+    st.write("| | -")
+    st.write("| | +")
+    st.write("| | nummbers")
