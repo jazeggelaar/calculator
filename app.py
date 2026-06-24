24dac03e3edeeb22b3e2a26c8b3147502f36bad9
jazeggelaar <jazeggelaar@gmail.com>
2026-06-24 14:13:57 +0000

    update

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st #jesse-calculator.streamlit.app
-rules_visibility = True
-st.title("calculator!")
-math = st.text_input("type here your math question")
-if st.button("run math"):
-    answer = eval(math)
-    st.write("calculated:", answer)
-    rules_visibility = False
-if rules_visibility:
+page = 1
+if page == 1:
+    st.title("calculator!")
+    math = st.text_input("type here your math question")
+    if st.button("run math"):
+        answer = eval(math)
+        st.write("calculated:", answer)
+    if st.button("rules"):
+        page = 2
+if page == 2:
     st.write("rules:")
     st.write("| unsupported:")
     st.write("| | letters")
@@ -17,3 +19,26 @@ if rules_visibility:
     st.write("| | -")
     st.write("| | +")
     st.write("| | nummbers")
+    if st.button("information about rules"):
+        page = 3
+    if st.button("i dont understand"):
+        page = 5
+    if st.button("ok"):
+        page = 1
+if page == 3:
+    st.write("[parent]:")
+    st.write("| [string]")
+    if st.button("extra information"):
+        page = 4
+    if st.button("back"):
+        page = 2
+if page == 4:
+    st.write(": as /")
+    st.write("x as *")
+    if st.button("back"):
+        page = 3
+if page == 5:
+    st.write("unsupported = not allowed")
+    st.write("supported = allowed")
+    if st.button("back"):
+        page = 2
