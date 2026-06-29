4c9ba97fe704358ec473741703423075f9577f3b
jazeggelaar <jazeggelaar@gmail.com>
2026-06-29 13:36:01 +0000

    moved

--- pages/information.py
+++ pages/information.py
@@ -2,6 +2,6 @@ import streamlit as st
 st.write("[text parent]:")
 st.write("| [text]")
 if st.button("extra information"):
-    st.switch_page("pages/information_extra.py")
+    st.switch_page("pages/extra information.py")
 if st.button("back"):
     st.switch_page("pages/rules.py")
--- pages/rules.py
+++ pages/rules.py
@@ -12,6 +12,6 @@ st.write("| | nummbers")
 if st.button("information about rules"):
     st.switch_page("pages/information.py")
 if st.button("i dont understand"):
-    st.switch_page("pages/understand_not.py")
+    st.switch_page("pages/not understand.py")
 if st.button("ok"):
     st.switch_page("app.py")
