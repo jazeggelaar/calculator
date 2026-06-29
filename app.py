d0d1e7e0d33dfe9fcbb57b2406b1d724cd38c871
jazeggelaar <jazeggelaar@gmail.com>
2026-06-29 14:57:46 +0000

    removed if

--- app.py
+++ app.py
@@ -2,10 +2,7 @@ import streamlit as st #jesse-calculator.streamlit.app
 st.title("calculator!")
 math = st.text_input("type here your math question")
 if st.button("run math"):
-    if type(math) == ("string"):
-        answer = eval(math)
-        st.write("calculated:", answer)
-    else:
-        st.write("check the rules again")
+    answer = eval(math)
+    st.write("calculated:", answer)
 if st.button("rules(click here before use)"):
     st.switch_page("pages/rules.py")
